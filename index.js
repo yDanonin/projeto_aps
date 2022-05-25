a076848e0595378f7bb35ea5d76c626f262100ae
Fabiano Barros <fabianorocha102003@gmail.com>
2022-05-25 11:29:56 -0300

    WIP calculadora

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ function lampada(novaImagem) {
   document.getElementById('textoLampada').textContent = 'Estamos aqui para conscientizar sobre o uso da enérgica elétrica! Você sabe o que são fontes renovaveis e fontes não renovaveis de energia?'
   document.getElementById('aviso').textContent = ''
 }
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
