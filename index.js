6a277dc354bbd60c188c55f90fdf8715dfdac1ab
Fabiano Barros <fabianorocha102003@gmail.com>
2022-05-25 10:22:03 -0300

    Add lampada action and adjust others sections

---
+++ index.js
@@ -0,0 +1,5 @@
+function lampada(novaImagem) {
+  document.getElementById('lampada').src = novaImagem
+  document.getElementById('textoLampada').textContent = 'Estamos aqui para conscientizar sobre o uso da enérgica elétrica! Você sabe o que são fontes renovaveis e fontes não renovaveis de energia?'
+  document.getElementById('aviso').textContent = ''
+}
