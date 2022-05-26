1d9f6173897eef2cc5bb39ae4012521f92dae2d8
Fabiano Barros <fabianorocha102003@gmail.com>
2022-05-25 22:29:33 -0300

    Create letterPerSecond function to the banner

--- index.js
+++ index.js
@@ -13,10 +13,30 @@ function closeForm() {
 }
 
 function calcExpense() {
-    var expense = document.getElementById("expense_calc").value;
-    var tariff = document.getElementById("tariff_calc").value;
+  var expense = document.getElementById("expense_calc").value;
+  var tariff = document.getElementById("tariff_calc").value;
 
-    result = (expense * tariff) * 30;
-    result_formtd = (Math.round(result *100) / 100).toFixed(2);
-    document.getElementById("texto_teste").textContent = " R$" + result_formtd;
+  result = (expense * tariff) * 30;
+  result_formtd = (Math.round(result *100) / 100).toFixed(2);
+  document.getElementById("texto_teste").textContent = " R$" + result_formtd;
+}
+
+function letterPerSecond(phrase) {
+  var interval
+  let arr = phrase.split('')
+  let i = 0
+  let word = ''
+  let nameId = 'phraseBanner1'
+  interval = setInterval(() => {
+    word = word+arr[i]
+    if (arr[i] == ' '){
+      nameId = 'phraseBanner2'
+      word = ''
+    } 
+    document.getElementById(nameId).textContent = word 
+    i++
+    if(i >= arr.length){
+      clearInterval(interval)
+    }
+  }, 100)
 }
